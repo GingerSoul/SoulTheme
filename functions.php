97a100eaa97cc34806f9e1a38dcc3c5e7e3ec578
Dave Bloom <gingersoulrecords@gmail.com>
2021-05-14 12:42:42 -0400

    adding theme support and list menu shortcode.

--- functions.php
+++ functions.php
@@ -16,12 +16,14 @@ function after_setup_theme()
 {
     load_theme_textdomain('gingertheme');
     add_theme_support('title-tag');
+    add_theme_support( 'menus' );
     add_theme_support('html5', [
         'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'script', 'style',
     ]);
     add_theme_support('fl-theme-builder-headers');
     add_theme_support('fl-theme-builder-footers');
     add_theme_support('fl-theme-builder-parts');
+	add_theme_support( 'post-thumbnails' );
 }
 
 add_action('wp', __NAMESPACE__ . '\\header_footer_render');
@@ -44,3 +46,41 @@ function get_theme_builder_callback($section)
     return FLThemeBuilderLayoutData::$method_name() ?
         [FLThemeBuilderLayoutRenderer::class, "render_$section"] : false;
 }
+
+function list_menu($atts, $content = null) {
+    extract(shortcode_atts(array(  
+        'menu'            => '', 
+        'container'       => '', 
+        'container_class' => '', 
+        'container_id'    => '', 
+        'menu_class'      => '', 
+        'menu_id'         => '',
+        'echo'            => true,
+        'fallback_cb'     => 'wp_page_menu',
+        'before'          => '',
+        'after'           => '',
+        'link_before'     => '',
+        'link_after'      => '',
+        'depth'           => 0,
+        'walker'          => '',
+        'theme_location'  => ''), 
+        $atts));
+  
+    return wp_nav_menu( array( 
+        'menu'            => $menu, 
+        'container'       => $container, 
+        'container_class' => $container_class, 
+        'container_id'    => $container_id, 
+        'menu_class'      => $menu_class, 
+        'menu_id'         => $menu_id,
+        'echo'            => false,
+        'fallback_cb'     => $fallback_cb,
+        'before'          => $before,
+        'after'           => $after,
+        'link_before'     => $link_before,
+        'link_after'      => $link_after,
+        'depth'           => $depth,
+        'walker'          => $walker,
+        'theme_location'  => $theme_location));
+}
+add_shortcode("listmenu", "list_menu");
