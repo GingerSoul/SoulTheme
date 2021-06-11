f300983888f4c905ebe8ed949d90aa4ab8395bb1
TomCarless <tom@fasterforward.com.au>
2021-06-11 11:15:05 -0700

    Fix double header tags

--- template-parts/default-header.php
+++ template-parts/default-header.php
@@ -1,6 +1,8 @@
-<p class="header-title">
-    <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-</p>
-<p class="header-description">
-    <?php bloginfo('description'); ?>
-</p>
+<header id="site-header" class="header-footer-group" role="banner">
+    <p class="header-title">
+        <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+    </p>
+    <p class="header-description">
+        <?php bloginfo('description'); ?>
+    </p>
+</header><!-- #site-header -->
